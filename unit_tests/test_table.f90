64cffa43cdeba8ad4641ff9406a15ee8fc865905
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-06-08 09:27:34 -0700

    add initial setup for table tests

---
+++ unit_tests/test_table.f90
@@ -0,0 +1,46 @@
+! This is a test program for UPP.
+!
+! This program tests the TABLE() subroutine.
+!
+! Alyson Stahl, 6/2026
+program test_table
+    implicit none
+
+    real, parameter :: tol = 1.0e-8
+    integer, parameter :: ITB=076, JTB=134
+    integer :: i, j, res
+    !
+    real :: PT, THL
+    real :: PTBL(ITB,JTB), TTBL(JTB,ITB)
+    real :: QS0(JTB), SQS(JTB), STHE(ITB), THE0(ITB)
+    real :: RDQ, RDTH, RDP, RDTHE, PL
+    !
+    real :: EXP_RDQ, EXP_RDTH, EXP_RDP, EXP_RDTHE, EXP_PL
+
+    interface
+        subroutine TABLE(PTBL, TTBL, PT, RDQ, RDTH, RDP, RDTHE,     &
+                        PL, THL, QS0, SQS, STHE, THE0)
+            real, intent(in)    :: PT, THL
+            real, dimension(ITB,JTB), intent(out)   :: PTBL
+            real, dimension(JTB,ITB), intent(out)   :: TTBL
+            real, dimension(JTB), intent(out)   :: QS0, SQS
+            real, dimension(ITB), intent(out)   :: STHE, THE0
+            real, intent(out)   :: RDQ, RDTH, RDP, RDTHE, PL
+        end subroutine TABLE
+    end interface
+
+    ! PL = PT
+    ! RDQ = KPM - 1 = ITB - 1 = 75
+    ! RDTH = 1./DTH -> DTH = (THH - THL) / REAL(KTHM - 1) = (THH - THL) / REAL(JTB - 1) = (365 - THL) / 133
+    ! RDP = 1./DP -> DP = (PH - PL) / REAL(KPM - 1) = (PH - PL) / REAL(ITB - 1) = (105000.- PL) / 75
+    ! RDTHE = 1./DTHE -> DTHE = 1 / REAL(KTHM - 1) = 1 / (JTB - 1) = 1 / 133
+
+    ! Same values used in INITPOST_GFS_NEMS_MPIIO()
+    PT = 10000.0
+    THL = 210.0
+
+    call TABLE(PTBL, TTBL, PT, RDQ, RDTH, RDP, RDTHE, PL, THL, QS0, SQS, STHE, THE0)
+
+
+    print *, 'SUCCESS!'
+end program test_table
---
+++ unit_tests/test_tableq.f90
@@ -0,0 +1,40 @@
+! This is a test program for UPP.
+!
+! This program tests the TABLEQ() subroutine.
+!
+! Alyson Stahl, 6/2026
+program test_tableq
+    implicit none
+
+    real, parameter :: tol = 1.0e-8
+    integer, parameter :: ITB=152, JTB=440
+    integer :: i, j, res
+    !
+    real :: PL, THL
+    real :: TTBLQ(JTB,ITB)
+    real :: STHE(ITB), THE0(ITB)
+    real :: RDP, RDTHE
+    !
+    real :: EXP_RDP, EXP_RDTHE
+
+    interface
+        subroutine TABLEQ(TTBLQ, RDP, RDTHE, PL, THL, STHE, THE0)
+            real, intent(in) :: PL, THL
+            real, dimension(JTB,ITB), intent(out) :: TTBLQ
+            real, dimension(ITB), intent(out) :: THE0, STHE
+            real, intent(out) :: RDP, RDTHE
+        end subroutine TABLEQ
+    end interface
+
+    ! RDP = 1./DP -> DP = (PH - PL) / REAL(KPM - 1) = (PH - PL) / REAL(ITB - 1) = (105000.- PL) / 75
+    ! RDTHE = 1./DTHE -> DTHE = 1 / REAL(KTHM - 1) = 1 / (JTB - 1) = 1 / 133
+
+    ! Same values used in INITPOST_GFS_NEMS_MPIIO()
+    PL = 70000.0
+    THL = 210.0
+
+    call TABLEQ(TTBLQ, RDP, RDTHE, PL, THL, STHE, THE0)
+
+
+    print *, 'SUCCESS!'
+end program test_tableq
