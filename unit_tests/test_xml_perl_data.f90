d66dea45055d1854e39e352b5cce89a0ae241c0e
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-07-24 13:56:06 -0700

    add test_xml_perl_data.f90 + set up

---
+++ unit_tests/test_xml_perl_data.f90
@@ -0,0 +1,55 @@
+! This is a test program for UPP.
+!
+! This program tests the subroutines in the xml_perl_data module.
+!
+! Alyson Stahl, 7/2026
+program test_xml_perl_data
+    use xml_perl_data
+    use rqstfld_mod, only: num_post_afld
+    use CTLBLK_mod, only: tprec, tclod, trdlw, trdsw, tsrfc, tmaxmin, td3d, filenameflat
+    implicit none
+
+    integer, parameter :: ntests = 3 ! This will also be the same 
+    integer :: i, j, res
+    character :: inpchar
+    !
+    integer :: EXP_NUM_POST_AFLD, EXP_PARAMSET_COUNT
+    real :: EXP_TPREC
+
+    ! XML file name
+    filenameflat = "data/ref_test_xml_perl_data.txt"
+
+    print *, "Testing filter_char_inp() subroutine..."
+
+    ! Test Case 1: Input is "?", should return empty string.
+    inpchar = "?"
+    call filter_char_inp(inpchar)
+
+    if (inpchar .ne. 0) then
+        print *, "Test Case 1 Failed: Input is '?', Result: ", inpchar
+        stop 10
+    end if
+
+    ! Test Case 2: Input should remain unchanged.
+    inpchar = "a"
+    call filter_char_inp(inpchar)
+    if (inpchar .ne. "a") then
+        print *, "Test Case 2 Failed: Input is 'a', Result: ", inpchar
+        stop 20
+    end if
+
+    print *, "Testing read_postxconfig() subroutine..."
+    
+    ! These variables only used in gen_proc_type == 'ens_fcst' case.
+    tprec = 2.
+    tclod = 1
+    trdlw = 1.
+    trdsw = 1.
+    tsrfc = 1.
+    tmaxmin = 1.
+    td3d = 1.
+
+    call read_postxconfig()
+    
+    print *, "SUCCESS!"
+end program test_xml_perl_data
