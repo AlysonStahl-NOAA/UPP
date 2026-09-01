8405b5d5918fb07ebab253933709574162d5b104
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-08-31 21:08:01 -0700

    Update test_derived_fields.f90

--- unit_tests/test_derived_fields.f90
+++ unit_tests/test_derived_fields.f90
@@ -43,34 +43,48 @@ program test_derived_fields
     cin = -50.0
     cape = 1800.0
 
+    exp_pc = 0.0
+    exp_kx = 34.2969360
+    exp_lx = -43.6572571
+    exp_tott = 60.0737305
+    exp_prcpType = PRECIPS%CONVECTION
+
+    exp_ept(1) = 3.78657196E+02
+    exp_ept(2) = 3.40894562E+02
+    exp_ept(3) = 3.22361389E+02
+    exp_ept(4) = 3.12390656E+02
+    exp_ept(5) = 3.07577484E+02
+    exp_ept(6) = 3.06790283E+02
+    exp_ept(7) = 3.10039032E+02
+    exp_ept(8) = 3.18258545E+02
+    exp_ept(9) = 3.33539337E+02
+    exp_ept(10) = 3.59922333E+02
+
+    exp_wbt(1) = 2.19768265E+02
+    exp_wbt(2) = 2.28567307E+02
+    exp_wbt(3) = 2.37400467E+02
+    exp_wbt(4) = 2.46090515E+02
+    exp_wbt(5) = 2.54864166E+02
+    exp_wbt(6) = 2.63451843E+02
+    exp_wbt(7) = 2.72300842E+02
+    exp_wbt(8) = 2.81353973E+02
+    exp_wbt(9) = 2.90605042E+02
+    exp_wbt(10) = 3.00208923E+02
+
+    exp_twp = 0.0
+    exp_twp(3:5) = 6.82362318E+00
+
     res = 0
     call derive_fields(imp_physics,t, rh, pres, hgt, totalWater, totalCond,&
                            nz, topoK, hprcp, hcprcp, cin, cape, &
                            ept, wbt, twp, pc, kx, lx, tott, prcpType)
 
-    if (prcpType .ne. PRECIPS%CONVECTION) then
-        print *, "Test Case 1 returned wrong precipitation type: ", prcpType
-        res = 1
-    end if
+    call check_expected_values(pc, kx, lx, tott, prcpType, ept, wbt, twp, &
+                               exp_pc, exp_kx, exp_lx, exp_tott, exp_prcpType, &
+                                exp_ept, exp_wbt, exp_twp, 1, res)
 
-    print *, "pc: ", pc
-    print *, "kx: ", kx
-    print *, "lx: ", lx
-    print *, "tott: ", tott
-    
-    do i = 1, nz
-        print '(A,I0,A,ES16.8)', "ept(", i, "): ", ept(i)
-    end do
-    
-    do i = 1, nz
-        print '(A,I0,A,ES16.8)', "wbt(", i, "): ", wbt(i)
-    end do
+    if (res .ne. 0) stop 10
 
-    do i = 1, nz
-        print '(A,I0,A,ES16.8)', "twp(", i, "): ", twp(i)
-    end do
-
-    print *, "End of Test Case 1"
     ! Test Case 2: Precipitation threshold fails (Precipitation type is None)
     imp_physics = 11
     hcprcp = 0.0
@@ -84,34 +98,37 @@ program test_derived_fields
     pres(8) = 80000.0
     pres(9) = 90000.0
 
+    exp_pc = 0.0
+    exp_kx = 39.6224976 
+    exp_lx = -39.1572571
+    exp_tott = 60.5223389 
+    exp_prcpType = PRECIPS%NONE
+
+    exp_ept(7) = 3.17146759E+02
+    exp_ept(8) = 3.25269409E+02
+    exp_ept(9) = 3.41011871E+02
+    exp_ept(10) = 3.59922333E+02
+
+    exp_wbt(6) = 2.63451843E+02
+    exp_wbt(7) = 2.72242249E+02
+    exp_wbt(8) = 2.81262512E+02
+    exp_wbt(9) = 2.90605042E+02
+    exp_wbt(10) = 3.00208923E+02
+
+    exp_twp = 0.0
+    exp_twp(3:5) = 6.57867432E+00
+
     res = 0
     call derive_fields(imp_physics,t, rh, pres, hgt, totalWater, totalCond,&
                            nz, topoK, hprcp, hcprcp, cin, cape, &
                            ept, wbt, twp, pc, kx, lx, tott, prcpType)
 
-    if (prcpType .ne. PRECIPS%NONE) then
-        print *, "Test Case 2 returned wrong precipitation type: ", prcpType
-        res = 1
-    end if
+    call check_expected_values(pc, kx, lx, tott, prcpType, ept, wbt, twp, &
+                               exp_pc, exp_kx, exp_lx, exp_tott, exp_prcpType, &
+                                exp_ept, exp_wbt, exp_twp, 1, res)
 
-    print *, "pc: ", pc
-    print *, "kx: ", kx
-    print *, "lx: ", lx
-    print *, "tott: ", tott
+    if (res .ne. 0) stop 20
     
-    do i = 1, nz
-        print '(A,I0,A,ES16.8)', "ept(", i, "): ", ept(i)
-    end do
-    
-    do i = 1, nz
-        print '(A,I0,A,ES16.8)', "wbt(", i, "): ", wbt(i)
-    end do
-
-    do i = 1, nz
-        print '(A,I0,A,ES16.8)', "twp(", i, "): ", twp(i)
-    end do
-    print *, "End of Test Case 2"
-
     ! getThetaw => gives  wbt
     !   i) dry air: td << t, rh << 100%, lower pres
     !   ii) moist air: td ~ t, rh ~ 100%, higher pres
@@ -156,5 +173,59 @@ program test_derived_fields
     !       a) coldTemp > 265.15
     !       b) tcoldTemp <= 265.15 & tColdArea >= 350.0 & wetBuldArea <= -250.0
     !       c) coldTemp <= 265.15 & tColdArea >= 350.0 & wetBuldArea > -250.0 & t(k) > 273.15
+
+    contains
+
+    subroutine check_expected_values(pc, kx, lx, tott, prcpType, ept, wbt, twp, &
+                                        exp_pc, exp_kx, exp_lx, exp_tott, exp_prcpType, &
+                                        exp_ept, exp_wbt, exp_twp, test_case, res)
+        implicit none
+        real, intent(in) :: pc, kx, lx, tott
+        integer, intent(in) :: prcpType
+        real, intent(in) :: ept(nz), wbt(nz), twp(:)
+        real, intent(in) :: exp_pc, exp_kx, exp_lx, exp_tott
+        integer, intent(in) :: exp_prcpType
+        real, intent(in) :: exp_ept(nz), exp_wbt(nz), exp_twp(:)
+        integer, intent(in) :: test_case
+        integer, intent(inout) :: res
+        integer :: i
+
+    if (prcpType .ne. exp_prcpType) then
+        print *, "Test Case ", test_case, " returned wrong precipitation type: ", prcpType
+        res = 1
+    end if
+
+    if (abs(pc - exp_pc) > tol) then
+        print *, "Test Case ", test_case, " expected pc = ", exp_pc, ", but got ", pc
+        res = 1
+    end if
+    if (abs(kx - exp_kx) > tol) then
+        print *, "Test Case ", test_case, " expected kx = ", exp_kx, ", but got ", kx
+        res = 1
+    end if
+    if (abs(lx - exp_lx) > tol) then
+        print *, "Test Case ", test_case, " expected lx = ", exp_lx, ", but got ", lx
+        res = 1
+    end if
+    if (abs(tott - exp_tott) > tol) then
+        print *, "Test Case ", test_case, " expected tott = ", exp_tott, ", but got ", tott
+        res = 1
+    end if
+
+    do i = 1, nz
+        if (abs(ept(i) - exp_ept(i)) > tol) then
+            print *, "Test Case ", test_case, " expected ept(", i, ") = ", exp_ept(i), ", but got ", ept(i)
+            res = 1
+        end if
+        if (abs(wbt(i) - exp_wbt(i)) > tol) then
+            print *, "Test Case ", test_case, " expected wbt(", i, ") = ", exp_wbt(i), ", but got ", wbt(i)
+            res = 1
+        end if
+        if (abs(twp(i) - exp_twp(i)) > tol) then
+            print *, "Test Case ", test_case, " expected twp(", i, ") = ", exp_twp(i), ", but got ", twp(i)
+            res = 1
+        end if
+    end do
+    end subroutine check_expected_values
 
 end program test_derived_fields
