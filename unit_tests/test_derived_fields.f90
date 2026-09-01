9fa7aece76db90a15c9ae83b0a85df8cbc12221b
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2026-08-31 23:14:21 -0700

    Update test_derived_fields.f90

--- unit_tests/test_derived_fields.f90
+++ unit_tests/test_derived_fields.f90
@@ -353,33 +353,47 @@ program test_derived_fields
     pres(8) = 84999.95
     pres(10) = 100000.0
 
+    exp_pc = 1.20000001E-02
+    exp_kx = 30.17388916
+    exp_lx = 23.35687256
+    exp_tott = 51.07373047
+    exp_prcpType = PRECIPS%OTHER
+
+    exp_ept(1) =     425.37011719
+    exp_ept(2) =     363.49542236
+    exp_ept(3) =     337.06890869
+    exp_ept(4) =     323.35174561
+    exp_ept(5) =     318.45806885
+    exp_ept(6) =     314.49588013
+    exp_ept(7) =     317.14682007
+    exp_ept(8) =     318.25857544
+    exp_ept(9) =     286.71722412
+    exp_ept(10) =     283.59588623
+    exp_wbt(1) =     219.53652954
+    exp_wbt(2) =     228.56730652
+    exp_wbt(3) =     237.40046692
+    exp_wbt(4) =     246.09051514
+    exp_wbt(5) =     255.84771729
+    exp_wbt(6) =     263.45184326
+    exp_wbt(7) =     272.24224854
+    exp_wbt(8) =     281.35397339
+    exp_wbt(9) =     269.36096191
+    exp_wbt(10) =     273.10000610
+    exp_twp = 0.0
+    exp_twp(3:5) = 6.06132603
+    exp_twp(8:10) = 8.77268791
 
     res = 0
     call derive_fields(imp_physics,t, rh, pres, hgt, totalWater, totalCond,&
                            nz, topoK, hprcp, hcprcp, cin, cape, &
                            ept, wbt, twp, pc, kx, lx, tott, prcpType)
 
-    print '(A,F16.8)', "pc = ", pc
-    print '(A,F16.8)', "kx = ", kx
-    print '(A,F16.8)', "lx = ", lx
-    print '(A,F16.8)', "tott = ", tott
-    print '(A,I0)', "prcpType = ", prcpType
-
-    do k = 1, nz
-        print '(A,I0,A,F16.8)', "ept(", k, ") = ", ept(k)
-    end do
-    do k = 1, nz
-        print '(A,I0,A,F16.8)', "wbt(", k, ") = ", wbt(k)
-    end do
-    do k = 1, nz
-        print '(A,I0,A,F16.8)', "twp(", k, ") = ", twp(k)
-    end do
     
-    !call check_expected_values(pc, kx, lx, tott, prcpType, ept, wbt, twp, &
-    !                           exp_pc, exp_kx, exp_lx, exp_tott, exp_prcpType, &
-    !                            exp_ept, exp_wbt, exp_twp, 11, res)
+    call check_expected_values(pc, kx, lx, tott, prcpType, ept, wbt, twp, &
+                               exp_pc, exp_kx, exp_lx, exp_tott, exp_prcpType, &
+                                exp_ept, exp_wbt, exp_twp, 11, res)
                                 
-    !if (res .ne. 0) stop 110
+    if (res .ne. 0) stop 110
 
     print *, "SUCCESS!"
 
